2444b89764aac4e2a8f2acb7479880115873f6a3
makhizu <79908795+makhizu@users.noreply.github.com>
2022-05-20 17:59:32 +0700

    Update Fakultas.dart

--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -10,10 +10,37 @@ class Fakultas extends StatelessWidget {
       child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
         InkWell(
           child: Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris 1"),
-          ),
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      // decoration: BoxDecoration(border: Border.all()),
+                      child: Column(
+                    children: [
+                      Text(
+                        "FPMIPA",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        "Fakultas Matematika dan Ilmu Pengetahuan Alam",
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  )),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                  )
+                ],
+              )),
           onTap: () {
             //gunakan navigator untuk panggil RincianFakultas
           },
@@ -21,7 +48,35 @@ class Fakultas extends StatelessWidget {
         Container(
             decoration: BoxDecoration(border: Border.all()),
             padding: EdgeInsets.all(14),
-            child: Text("baris kedua")),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "FPIPS",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        "Fakultas Pendidikan Ilmu Pengetahuan Sosial",
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                )
+              ],
+            )),
       ]),
     );
   }
