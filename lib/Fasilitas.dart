49923072a75668642e4d77d5f2b4e450b6c06eb3
makhizu <79908795+makhizu@users.noreply.github.com>
2022-05-20 17:59:55 +0700

    Update Fasilitas.dart

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -1,11 +1,130 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('fasilitas!'),
+    return Center(
+      child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
+        InkWell(
+          child: Container(
+              decoration: BoxDecoration(border: Border.all()),
+              padding: EdgeInsets.all(14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      // decoration: BoxDecoration(border: Border.all()),
+                      child: Column(
+                    children: [
+                      Text(
+                        "Perpus UPI",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ],
+                  )),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                  )
+                ],
+              )),
+          onTap: () {
+            //gunakan navigator untuk panggil RincianFakultas
+          },
+        ),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "GYM",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                ),
+              ],
+            )),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Kolam Renang",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                ),
+              ],
+            )),
+        Container(
+            decoration: BoxDecoration(border: Border.all()),
+            padding: EdgeInsets.all(14),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Text(
+                        "Sport Hall",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'))),
+                ),
+              ],
+            ))
+      ]),
     );
   }
 }
